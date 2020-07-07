95cdf7fd8dcd4135d7902634859c658a7f531013
hongmin <tmxkzm35@naver.com>
2020-07-07 13:42:31 +0900

    7/7

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcrypt');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const { User } = require('../models');
 
-const router = express.Router();
 
+const router = express.Router();
 
 router.post('/join', isNotLoggedIn, async (req, res, next) => {
   const { email, nick, password } = req.body;
